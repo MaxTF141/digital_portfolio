3e08e9df11bf3741681ddecc3d1752b99278f093
MaxTF141 <isaacsmaxwill22@gmail.com>
2023-05-05 16:31:30 +0200

    'skills'

--- src/components/SkillsComponent.vue
+++ src/components/SkillsComponent.vue
@@ -2,45 +2,14 @@
     <section id="skills">
         <div class="container-fluid">
             <h1>SKILLS</h1>
-            <div class="row d-flex justify-content-center py-3">
-                <div class="col-md-3 col-12">
-                    <div class="html-card d-flex justify-content-center align-items-center">
-                        <img class="" src="https://i.postimg.cc/HsQ9SmYK/ri-html5-line.png" alt="">
-                    </div>
-                </div>
-                <div class="col-md-3 col-12">
-                    <div class="vue-card d-flex justify-content-center align-items-center">
-                        <img class="" src="https://i.postimg.cc/gk9KqjW8/logos_vue.png" alt="">
-                    </div>
-                </div>
-                <div class="col-md-3 col-12 ">
-                    <div class="js-card d-flex justify-content-center align-items-center">
-                        <img class="" src="https://i.postimg.cc/fWSK9X6M/logos_javascript.png" alt="">
-                    </div>
-                </div>
-            </div>
-            <div class="row d-flex justify-content-center p-3">
-                <div class="col-md-3 col-12">
-                    <div class="css-card d-flex justify-content-center align-items-center">
-                        <img class="" src="https://i.postimg.cc/NfZMyrGV/uiw_css3.png" alt="">
-                    </div>
-                </div>
-                <div class="col-md-3 col-12 ">
-                    <div class="bootstrap-card d-flex justify-content-center align-items-center">
-                        <img class="" src="https://i.postimg.cc/3NXTdWLM/fa6-brands_bootstrap_(1).png" alt="">
-                    </div>
-                </div>
-                <div class="col-md-3 col-12">
-                    <div class="node-card d-flex justify-content-center align-items-center">
-                        <img class="" src="https://i.postimg.cc/1RC4Qjky/simple-icons_nodedotjs.png" alt="">
-                    </div>
-                </div>
-            </div>
-            <div class="row d-flex justify-content-center py-3">
-                <div class="col-md-3 col-12">
-                    <div class="mysql-card d-flex justify-content-center align-items-center">
-                        <img src="https://i.postimg.cc/Kv1jv5bB/logos_mysql.png" alt="">
-                    </div>
+            <div class="row">
+                <div class="col-12">
+                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAg0lEQVR4nH3QwQmDQBCF4c3VFgJpw4ONKPGwp7Sx6SUpRbCOkBbEY/4w+hJmZfXBwDLzwQ4TgB64qSJwUkXX7wNwJ08LdJteMlgBb9d8qX6xWRUs+n4v1wUJ2k5DAY02+0PhGvg4ZO8mQw4/HXwUkeAZmIAZuOxCYTtXOkSCdq71HC5fr87otu0tkpcAAAAASUVORK5CYII=">
+                    <i class="fa-brands fa-vuejs" style="color: #ffffff;"></i>
+                    <i class="fa-brands fa-html5" style="color: #ffffff;"></i>
+                    <i class="fa-brands fa-css3-alt" style="color: #ffffff;"></i>
+                    <i class="fa-brands fa-square-js" style="color: #ffffff;"></i>
+                    <i class="fa-brands fa-node-js" style="color: #ffffff;"></i>
                 </div>
             </div>
         </div>
@@ -59,54 +28,9 @@ export default {
     padding: 0;
     box-sizing: border-box;
 }
-.html-card{
-    background: #FC490B;
-    border-radius: 18px;
-    box-shadow: 3px 3px 7px #bbbbbb;
-    width: 80%;
-    height: 100%;
-}
-.vue-card{
-    background: #41B883;
-    border-radius: 18px;
-    box-shadow: 3px 3px 7px #bbbbbb;
-    width: 80%;
-    height: 100%;
-}
-.js-card{
-    background: #FFDF00;
-    border-radius: 18px;
-    box-shadow: 3px 3px 7px #bbbbbb;
-    width: 80%;
-    height: 100%;
-}
-.css-card{
-    background: #2196F3;
-    border-radius: 18px;
-    box-shadow: 3px 3px 7px #bbbbbb;
-    width: 80%;
-    height: 100%;
-}
-.bootstrap-card{
-    background: #5E3B7F;
-    border-radius: 18px;
-    box-shadow: 3px 3px 7px #bbbbbb;
-    width: 80%;
-    height: 100%;
-}
-.node-card{
-    background: #4F9640;
-    border-radius: 18px;
-    box-shadow: 3px 3px 7px #bbbbbb;
-    width: 80%;
-    height: 100%;
-}
-.mysql-card{
-    background: #00758F;
-    border-radius: 18px;
-    box-shadow: 3px 3px 7px #bbbbbb;
-    width: 80%;
-    height: 100%;
+
+#skills {
+    background-color: #ff0044;
 }
 #skills h1 {
   font-family: 'Ubuntu Mono', monospace;
@@ -114,8 +38,9 @@ export default {
   font-size: 11em;
 
 }
-img {
+i {
     padding: 20px;
+    font-size: 100px;
 }
 
 </style>
